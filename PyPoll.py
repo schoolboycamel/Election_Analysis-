ac7e4e9b28d36cb157bf1b73856e2fa37c43dd65
Paolo Espinoza <paoloaediaz@gmail.com>
2022-03-06 16:45:41 -0500

    Election_Analysis

---
+++ PyPoll.py
@@ -0,0 +1,12 @@
+import csv
+import os 
+file_to_load = os.path.join("Resources", "election_results.csv")
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+file_to_save = os.path.join('analysis', 'election_analysis.txt')
+with open(file_to_save, 'w') as txt_file:
+    txt_file.write("Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson")
+
+
