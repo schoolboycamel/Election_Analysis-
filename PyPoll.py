77d001b87025679ae266350db3013feeacfd8463
Paolo Espinoza <paoloaediaz@gmail.com>
2022-03-06 20:46:47 -0500

    Election_Analysis

--- PyPoll.py
+++ PyPoll.py
@@ -1,11 +1,40 @@
 import csv
 import os 
 file_to_load = os.path.join("Resources", "election_results.csv")
+file_to_save = os.path.join('analysis', 'election_analysis.txt')
+total_votes= 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
-    print(headers)
-file_to_save = os.path.join('analysis', 'election_analysis.txt')
+    for row in file_reader:
+        total_votes += 1 
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name]= 0
+        candidate_votes[candidate_name] += 1
+print(candidate_votes)    
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = round(float(votes) / float(total_votes) * 100, 2)
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+         winning_count = votes
+         winning_percentage = vote_percentage
+         winning_candidate = candidate_name
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 with open(file_to_save, 'w') as txt_file:
     txt_file.write("Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson")
 
